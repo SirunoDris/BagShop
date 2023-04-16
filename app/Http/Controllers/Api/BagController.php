276bf6019558cb652e62ec17f92175fdf7a7dc71
SirunoDris <j.sirunodri.elluch@gmail.com>
2023-04-17 01:29:23 +0200

    First commit.

---
+++ app/Http/Controllers/Api/BagController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Bag;
+use Illuminate\Http\Request;
+
+class BagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $bag = Bag::All();
+        return $bag;
+    }
+
+    /**
+     * Create a new Bag.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'material' => 'required',
+            'user_id' => 'required',
+        ]);
+    
+        $bag = new Bag;
+        $bag->name = $request->name;
+        $bag->price = $request->price;
+        $bag->material = $request->material;
+        $bag->user_id = $request->user_id;
+        $bag->save();
+
+        return response()->json([
+            'message'=> 'creado correctamente',
+            'bag'=>$bag
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bag  $bag
+     * @return \Illuminate\Http\Response
+     */
+    public function read($id)
+    {
+        $bag = Bag::findOrFail($id);
+        return $bag;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bag  $bag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bag $bag)
+    {
+        //Esta mal ¡, lo que esta jacienddo es crea rmas bolso
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'material' => 'required',
+            'user_id' => 'required',
+        ]);
+
+        $bag->name = $request->name;
+        $bag->price = $request ->price;
+        $bag->material = $request ->material;
+        $bag->user_id = $request->user_id;
+        $bag->save();
+
+        //$bags = Bag::update($request);
+        return "Bag ACTUALIZADO correctamente";
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bag  $bag
+     * @return \Illuminate\Http\Response
+     */
+    public function delete(Bag $bag)
+    {
+        $bag->delete();
+        return "Bag Borrado";
+
+    }
+}
---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\PersonalAccessToken;
+
+
+class LoginController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        
+        $user->save();
+        return response()->json([
+            'message' => 'Successfully created user'
+        ], 201);
+
+        
+    }
+    /**
+     * Login user and create token
+     *
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [boolean] remember_me
+     * @return [string] access_token
+     * @return [string] token_type
+     * @return [string] expires_at
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials))
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+
+        $user = $request->user();
+        $tokenResult = $user->createToken($request->name)->plainTextToken;
+      
+        //dd($tokenResult);
+        //dd($tokenResult->plainTextToken);
+        //$token = PersonalAccessToken::findToken($tokenId);
+
+        return response()->json([
+            'token' => $tokenResult,
+            'token_type' => 'Bearer',
+            'message' => 'Success'
+        ]);
+    }
+
+    /**
+     * Logout user (Revoke the token)
+     *
+     * @return [string] message
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    /**
+     * Get the authenticated User
+     *
+     * @return [json] user object
+     */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
